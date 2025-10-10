5aff56b7e8e9672de5b964b76a446499631eed53
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-10-10 08:36:15 -0600

    Scaffold PkgPageHeader(s)

---
+++ src/Marqdouj.DotNet.Demo/Marqdouj.DotNet.Demo/Components/Pages/HRefGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace Marqdouj.DotNet.Demo.Components.Pages
+{
+    internal enum HRefCodeSource
+    {
+        General
+    }
+
+    internal static class HRefGenerator
+    {
+        public static string CodeUrl(this HRefCodeSource source, string path)
+        {
+            string? url;
+
+            switch (source)
+            {
+                case HRefCodeSource.General:
+                    url = "https://github.com/marqdouj/dotnet.general/blob/master/src/Marqdouj.DotNet.General/Marqdouj.DotNet.General";
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return Path.Combine(url, path);
+        }
+     }
+}
