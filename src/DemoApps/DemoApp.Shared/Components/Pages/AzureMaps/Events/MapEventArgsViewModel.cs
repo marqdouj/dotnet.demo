6e603c69429e7ad268daf8505e7c33b6afb706e6
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-12-25 16:50:22 -0700

    Added EventViewer to Events pages.

---
+++ src/DemoApps/DemoApp.Shared/Components/Pages/AzureMaps/Events/MapEventArgsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Marqdouj.DotNet.AzureMaps.Map.Events;
+
+namespace DemoApp.Shared.Components.Pages.AzureMaps.Events
+{
+    public class MapEventArgsViewModel(MapEventArgs args)
+    {
+        public MapEventArgs Args { get; } = args;
+        public string Value { get; set; } = args.ToString();
+        public TimeSpan TimeStamp { get; set; } = new TimeSpan(DateTime.Now.Ticks);
+    }
+}
