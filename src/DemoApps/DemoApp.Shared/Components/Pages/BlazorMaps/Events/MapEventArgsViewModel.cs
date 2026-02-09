21e51e414829999678ad475ca746499b999b3009
Douglas Marquardt <marqdouj@users.noreply.github.com>
2026-02-09 11:14:56 -0700

    Update to release package versions and finished added BzMaps pages

---
+++ src/DemoApps/DemoApp.Shared/Components/Pages/BlazorMaps/Events/MapEventArgsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Marqdouj.DotNet.AzureMaps.Blazor.Models.Events;
+using System.Collections.ObjectModel;
+
+namespace DemoApp.Shared.Components.Pages.BlazorMaps.Events
+{
+    public class MapEventArgsViewModelManager
+    {
+        private readonly List<MapEventArgsViewModel> _items = [];
+
+        public MapEventArgsViewModelManager(int maxItems = 25)
+        {
+            Items = new ReadOnlyCollection<MapEventArgsViewModel>(_items);
+            MaxItems = maxItems;
+        }
+
+        public void Add(MapEventArgs args)
+        {
+            _items.Insert(0, new MapEventArgsViewModel(args));
+            while (_items.Count > MaxItems)
+            {
+                _items.RemoveAt(_items.Count - 1);
+            }
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public IReadOnlyCollection<MapEventArgsViewModel> Items { get; }
+        public int MaxItems { get; }
+    }
+
+    public class MapEventArgsViewModel(MapEventArgs args)
+    {
+        public MapEventArgs Args { get; } = args;
+        public string Value { get; set; } = args.ToJsonMin();
+        public TimeSpan TimeStamp { get; set; } = new TimeSpan(DateTime.Now.Ticks);
+    }
+}
---
+++ src/DemoApps/DemoApp.Shared/Components/Pages/BlazorMaps/Events/MapEventViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Marqdouj.DotNet.AzureMaps.Blazor.Models.Events;
+
+namespace DemoApp.Shared.Components.Pages.BlazorMaps.Events
+{
+    internal class MapEventViewModel
+    {
+        public MapEventViewModel(MapEventDef eventDef)
+        {
+            EventDef = eventDef;
+            EventDef.PreventDefault = true;
+        }
+
+        public MapEventDef EventDef { get; }
+        public string? Name => EventDef.Type.ToString();
+        public MapEventType? Type => EventDef.Type;
+        public bool IsChecked { get; set; }
+        public bool IsNotChecked => !IsChecked;
+        public bool IsLoaded { get; set; }
+        public bool IsNotLoaded => !IsLoaded;
+
+
+    }
+}
